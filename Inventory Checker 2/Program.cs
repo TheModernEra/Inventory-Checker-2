e5a76f2cb0c455c07d6c006d200f9067b4b82ee3
Bennett Rosenthal <bsrosenthal101@gmail.com>
2020-10-03 18:35:04 -0700

    Add project files.

---
+++ Inventory Checker 2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Inventory_Checker_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
