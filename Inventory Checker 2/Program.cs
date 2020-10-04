94fda279fcf91db5075fc54b5cd6762bc109a69f
Bennett Rosenthal <bsrosenthal101@gmail.com>
2020-10-03 23:34:59 -0700

    finished the thing

--- Inventory Checker 2/Program.cs
+++ Inventory Checker 2/Program.cs
@@ -27,8 +27,10 @@ namespace Inventory_Checker_2
                     Console.WriteLine("help - lists all commands available");
                     Console.WriteLine("list - lists all items added to the inventory");
                     Console.WriteLine("add - starts process to add an item to the inventory");
+                    Console.WriteLine("clear - clears list of items in inventory");
                     Console.WriteLine("exit - exits the program");
                     Console.WriteLine("");
+                    Console.WriteLine("Please enter another command.");
                 }
 
                 if (command.Equals("exit"))
@@ -44,17 +46,47 @@ namespace Inventory_Checker_2
                     Console.WriteLine("What is the item classified as? (appliance, utility, entertainment, etc.)");
                     var itemtype = Console.ReadLine();
 
-                    var initialJson = File.ReadAllText(@"c:\items.json");
-                    var array = JArray.Parse(initialJson);
+                    using (System.IO.StreamWriter file = new System.IO.StreamWriter(@"C:\items.txt", true))
+                    {
+                        file.WriteLine("Name: " + itemname + "\n" + "Type: "  + itemtype + "\n");
+                    }
 
+                    Console.WriteLine("Item added!");
+                    Console.WriteLine("Please enter another command.");
+                }
 
-                    Item example = new Item();
-                    example.Itemname = itemname;
-                    example.Itemtype = itemtype;
+                if (command.Equals("clear"))
+                {
+                    Console.WriteLine("Are you sure?");
+                    var uhoh = Console.ReadLine();
+                    if (uhoh.Equals("yes"))
+                    {
+                        File.Delete(@"C:\items.txt");
+                        Console.WriteLine("Inventory cleared!");
+                        Console.WriteLine("Please enter another command.");
+                    } else
+                    {
+                        Console.WriteLine("Inventory not cleared.");
+                        Console.WriteLine("Please enter another command.");
+                    }
+                }
 
-                    string objjsonData = JsonConvert.SerializeObject(example);
-                    Console.Write(objjsonData);
-                    System.IO.File.WriteAllText(@"C:\items.json", objjsonData);
+                if (command.Equals("list"))
+                {
+                    if (File.Exists(@"c:\items.txt") == false)
+                    {
+                        Console.WriteLine("No items have been added! Add one using the \"add\" command");
+                        Console.WriteLine("Please enter another command.");
+                    } else
+                    {
+                        string[] lines = System.IO.File.ReadAllLines(@"C:\items.txt");
+                        foreach (string line in lines)
+                        {
+                            Console.WriteLine(line);
+                        }
+                        Console.WriteLine("");
+                        Console.WriteLine("Please enter another command.");
+                    }
                 }
                 command = Console.ReadLine();
             }
