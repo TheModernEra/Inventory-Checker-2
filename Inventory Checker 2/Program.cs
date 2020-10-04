24e4766cd571205e24456e0fb99b1703b51cd626
Bennett Rosenthal <bsrosenthal101@gmail.com>
2020-10-03 18:37:36 -0700

    initial code added

--- Inventory Checker 2/Program.cs
+++ Inventory Checker 2/Program.cs
@@ -6,7 +6,32 @@ namespace Inventory_Checker_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Inventory Manager by Bennett Rosenthal");
+            Console.WriteLine("Enter a command to get started! type \"help\" for a list of commands :)");
+            var command = Console.ReadLine();
+            var checker = true;
+
+            while (checker == true)
+            {
+                if (command.Equals("help"))
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine("help - lists all commands available");
+                    Console.WriteLine("list - lists all items added to the inventory");
+                    Console.WriteLine("add - starts process to add an item to the inventory");
+                    Console.WriteLine("exit - exits the program");
+                    Console.WriteLine("");
+                }
+
+                if (command.Equals("exit"))
+                {
+                    checker = false;
+                    Console.WriteLine("Press any key to exit. See you next time!");
+                    Console.WriteLine("yuh");
+                }
+
+                command = Console.ReadLine();
+            }
         }
     }
 }
